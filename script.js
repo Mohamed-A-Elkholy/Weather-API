4833e061368830daf75a9de2d01628c401c35025
Mohamed-Elkholy <mohamedelkholy260@gmail.com>
2023-09-11 01:20:41 +0200

    Reuploading the files

--- script.js
+++ script.js
@@ -7,43 +7,35 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon= document.querySelector(".weather-icon");
 
 async function checkWeather(city){
-    const respose = await fetch(apiUrl +city + `&appid=${apiKey}`);
-    var data = await respose.json();
+    const response = await fetch(apiUrl +city + `&appid=${apiKey}`);
+    var data = await response.json();
 
-    if(respose.status === 404 || respose.status === undefined){
+     if(response.status === 404 || response.status === undefined){
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
-    }else{
-        document.querySelector(".error").style.display = "none";
-        
-    }
+     }else{
+        document.querySelector(".error").style.display = "none"; 
+     }
     
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-    
+    /*
     if(data.weather[0].main === "Clear"){
         weatherIcon.src = "./images/clear.png"
     }else if(data.weather[0].main === "Clouds"){
         weatherIcon.src = "./images/clear.png"
-        
     }else if(data.weather[0].main === "Drizzle"){
         weatherIcon.src = "./images/drizzle.png"
-        
     }else if(data.weather[0].main === "Mist"){
         weatherIcon.src = "./images/mist.png"
-        
     }else if(data.weather[0].main === "Rain"){
         weatherIcon.src = "./images/rain.png"
-        
     }else if(data.weather[0].main === "Snow"){
-        weatherIcon.src = "./images/snow.png"
-        
-    }
-
-    console.log(data);
-
+        weatherIcon.src = "./images/snow.png"        
+    }*/
+    console.log(data)
 }
 searchBtn.addEventListener("click",()=>{
     document.querySelector(".weather").style.display = "block";
